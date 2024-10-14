d56b653ab0f854dfc67c5c40609f1f7c4cb07832
SpiralGP <george@spiralcorp.co.uk>
2024-10-14 03:26:23 +0100

    Added react-router-dom, Routes, Rating and Product Screen

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
+import { LinkContainer } from "react-router-bootstrap";
 import logo from "../assets/logo.png";
 
 const header = () => {
@@ -7,19 +8,25 @@ const header = () => {
     <header>
       <Navbar bg="dark" variant="dark" expand="sm" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">
-            {/* <img className="logo" src={logo} alt="SpiralShop" /> */}
-            Spiral Shop
-          </Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>
+              <img className="logo" src={logo} alt="SpiralShop" />
+              SpiralShop
+            </Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="/cart">
-                <FaShoppingCart /> Cart
-              </Nav.Link>
-              <Nav.Link href="/login">
-                <FaUser /> Sign In
-              </Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <FaShoppingCart /> Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <FaUser /> Sign In
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
