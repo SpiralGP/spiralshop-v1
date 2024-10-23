a9cf0c345cc01102869ab2d5c268729a659bbaf2
SpiralGP <george@spiralcorp.co.uk>
2024-10-23 12:07:08 +0100

    Install Helmet, Meta Page titles

---
+++ frontend/src/components/Meta.jsx
@@ -0,0 +1,19 @@
+import { Helmet } from "react-helmet-async";
+
+const Meta = ({ title, description, keywords }) => {
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="keywords" content={keywords} />
+    </Helmet>
+  );
+};
+
+Meta.defaultProps = {
+  title: "Welcome to Spiral Shop",
+  description: "Shop whatever you want to shop",
+  keywords: "tip top, high end, good quality, great stuff",
+};
+
+export default Meta;
